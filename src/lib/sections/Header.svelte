7329a652a13b906abc4fe388cb04c65ffa25e2a4
Gabriel-RQ <123425138+Gabriel-RQ@users.noreply.github.com>
2026-01-26 15:08:59 -0300

    feat: adiciona cabeçalho

---
+++ src/lib/sections/Header.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+  import logo from "$lib/assets/logo-text.svg";
+</script>
+
+<header class="flex justify-center items-center">
+  <img
+    src={logo}
+    alt="Encontro nacional em Jaraguá"
+    class="scale-75 md:scale-100"
+  />
+</header>
