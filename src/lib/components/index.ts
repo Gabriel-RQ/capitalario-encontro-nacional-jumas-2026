3a732e907d6174593703b87e34b0727f67800fd3
Gabriel-RQ <123425138+Gabriel-RQ@users.noreply.github.com>
2026-01-26 16:42:29 -0300

    fix(components): renomeia PrayerCounter para PrayerCounterButtons

--- src/lib/components/index.ts
+++ src/lib/components/index.ts
@@ -2,4 +2,4 @@ export { default as Button } from "./Button.svelte";
 export { default as Card } from "./Card.svelte";
 export { default as Counter } from "./Counter.svelte";
 export { default as ProgressBar } from "./ProgressBar.svelte";
-export { default as PrayerCounter } from "./PrayerCounter.svelte";
+export { default as PrayerCounterButtons } from "./PrayerCounterButtons.svelte";
