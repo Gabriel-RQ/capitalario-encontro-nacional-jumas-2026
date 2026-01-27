2c4ef5f7aa5db38988c7851b2b7828a5fa4d330f
Gabriel-RQ <123425138+Gabriel-RQ@users.noreply.github.com>
2026-01-27 15:04:09 -0300

    feat: adiciona componente CardList

---
+++ src/lib/components/CardList.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+  import type { Snippet } from "svelte";
+
+  type CardListProps = {
+    children: Snippet<[]>;
+  };
+
+  let { children }: CardListProps = $props();
+</script>
+
+<div class="flex flex-col gap-5">
+  {@render children()}
+</div>
+
+<style>
+  div {
+    counter-reset: card-item;
+  }
+</style>
