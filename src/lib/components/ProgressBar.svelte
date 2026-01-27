25b338639f5b56e4496ffc249e318076d675ec1c
Gabriel-RQ <123425138+Gabriel-RQ@users.noreply.github.com>
2026-01-27 11:56:55 -0300

    update(components): pequeno ajuste

--- src/lib/components/ProgressBar.svelte
+++ src/lib/components/ProgressBar.svelte
@@ -11,5 +11,5 @@
 <div
   style="--progress: {(value / max) * 100}%"
   class="relative border-variation-dark-blue border-2 bg-variation-dark-blue/70 rounded-full appearance-none accent-amber-400 h-6
-  before:absolute before:inset-0 before:bg-linear-90 before:from-[#27A7DF] before:to-[#74A1B4] before:to-75% before:rounded-full before:w-(--progress) {className}"
+  before:absolute before:inset-px before:bg-linear-90 before:from-[#27A7DF] before:to-[#74A1B4] before:to-75% before:rounded-full before:w-(--progress) {className}"
 ></div>
