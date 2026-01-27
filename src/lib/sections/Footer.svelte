777e84f0b40bac0c71ac2106d452e0b3a6ebc5b5
Gabriel-RQ <123425138+Gabriel-RQ@users.noreply.github.com>
2026-01-27 15:05:17 -0300

    fix(sections): ajusta margem do rodapé

--- src/lib/sections/Footer.svelte
+++ src/lib/sections/Footer.svelte
@@ -1,4 +1,4 @@
-<footer class="text-center">
+<footer class="text-center mt-6">
   <p class="font-normal text-secondary text-sm text-balance">
     Copyright &copy; 2025. Todos os direitos reservados para Jumas Brasil.
   </p>
