9c39923c876dae6fd29cc2c1343013ef7caed09e
Gabriel-RQ <123425138+Gabriel-RQ@users.noreply.github.com>
2026-01-27 14:29:46 -0300

    feat: adiciona compomente Comment

---
+++ src/lib/components/Comment.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+  import type { Snippet } from "svelte";
+
+  type CommentProps = {
+    title: string;
+    children: Snippet<[]>;
+  };
+
+  let { title, children }: CommentProps = $props();
+</script>
+
+<div
+  class="border-2 border-primary flex flex-col text-primary gap-2 lg:gap-4 px-6 py-4 lg:px-8 lg:py-6 rounded-[60px] rounded-tr-none"
+>
+  <span class="font-bold text-lg lg:text-2xl">{title}</span>
+
+  {@render children()}
+</div>
