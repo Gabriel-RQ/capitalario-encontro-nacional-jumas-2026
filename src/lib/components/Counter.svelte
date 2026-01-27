18f26ecbb497f2d8114a81420d47607b4b1202ed
Gabriel-RQ <123425138+Gabriel-RQ@users.noreply.github.com>
2026-01-27 10:22:00 -0300

    feat: adiciona componente Counter

---
+++ src/lib/components/Counter.svelte
@@ -0,0 +1,47 @@
+<script lang="ts">
+  type CounterProps = {
+    current?: number;
+    maxLength: number;
+  };
+
+  let { current = 0, maxLength }: CounterProps = $props();
+
+  const formatter = new Intl.NumberFormat("pt-br", {
+    useGrouping: true,
+    // svelte-ignore state_referenced_locally
+    minimumIntegerDigits: maxLength,
+  });
+
+  // vai falhar quando chegar aos 8 dígitos, retornando um <span> + número (sem fechar o span)
+  let value = $derived.by(() => {
+    const parts = formatter
+      .format(
+        current
+          .toString()
+          .padStart(maxLength, "0") as Intl.StringNumericLiteral,
+      )
+      .split("");
+    let closed = false;
+
+    return parts
+      .map((part, i) => {
+        if (i === 0) return `<span class="text-variation-red/65">${part}`;
+        if (Number.parseInt(part) > 0 && !closed) {
+          closed = true;
+          return `</span>${part}`;
+        }
+        return part;
+      })
+      .join("");
+  });
+</script>
+
+<div
+  class="text-primary font-light text-sm lg:text-xl text-center gap-y-3 inline-flex flex-col"
+>
+  <span class="font-extrabold text-5xl lg:text-8xl">
+    {@html value}
+  </span>
+
+  <span> Orações do registradas </span>
+</div>
