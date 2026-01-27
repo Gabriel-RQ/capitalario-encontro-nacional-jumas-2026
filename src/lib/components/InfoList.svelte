133496f352a8ee472775576574ec2c916dccf21b
Gabriel-RQ <123425138+Gabriel-RQ@users.noreply.github.com>
2026-01-27 14:30:06 -0300

    feat: adiciona componente InfoList

---
+++ src/lib/components/InfoList.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+  type InfoListProps = {
+    items: string[];
+  };
+
+  let { items }: InfoListProps = $props();
+</script>
+
+<div class="flex flex-col gap-4 text-primary">
+  {#each items as item}
+    <span
+      class="inline-flex items-center gap-3 px-4 py-2 rounded-[80px] rounded-tr-none before:rounded-full before:flex before:items-center before:justify-center before:size-12 before:bg-primary before:font-bold before:text-background before:text-lg lg:before:text-3xl shadow-md"
+    >
+      {item}
+    </span>
+  {/each}
+</div>
+
+<style>
+  div {
+    counter-reset: list-item;
+  }
+
+  span::before {
+    content: counter(list-item);
+    counter-increment: list-item;
+  }
+</style>
