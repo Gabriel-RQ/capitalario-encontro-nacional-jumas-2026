c673e9688b6a4d5cbd28209efb2e1a04c6f8d93e
Gabriel-RQ <123425138+Gabriel-RQ@users.noreply.github.com>
2026-01-27 15:03:58 -0300

    feat: adiciona componente Card

---
+++ src/lib/components/Card.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+  import type { Snippet } from "svelte";
+
+  type CardProps = {
+    children: Snippet<[]>;
+  };
+
+  let { children }: CardProps = $props();
+</script>
+
+<div
+  class="flex flex-col border-2 items-center border-secondary rounded-[60px] rounded-tr-none px-6 py-4 before:size-15 lg:before:size-20 before:flex before:items-center before:justify-center before:bg-secondary before:rounded-full before:text-background before:font-bold before:text-lg lg:before:text-3xl before:border-variation-light-blue before:border-2 text-secondary gap-4 bg-background"
+>
+  <span class="text-base lg:text-xl">
+    {@render children()}
+  </span>
+</div>
+
+<style>
+  div::before {
+    content: counter(card-item);
+    counter-increment: card-item;
+  }
+</style>
