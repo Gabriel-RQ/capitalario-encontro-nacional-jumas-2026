fc64dd17fcca5aea03ef8c56e3f4941eeff8e7ea
Gabriel-RQ <123425138+Gabriel-RQ@users.noreply.github.com>
2026-01-28 10:17:37 -0300

    feat: adiciona componente ColorBand

---
+++ src/lib/components/ColorBand.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+  type ColorBandProps = {
+    class: string;
+  };
+
+  let { class: className }: ColorBandProps = $props();
+</script>
+
+<div class="flex h-3 *:flex-1 {className}">
+  <div class="bg-primary"></div>
+  <div class="bg-secondary"></div>
+  <div class="bg-tertiary"></div>
+  <div class="bg-variation-orange"></div>
+</div>
