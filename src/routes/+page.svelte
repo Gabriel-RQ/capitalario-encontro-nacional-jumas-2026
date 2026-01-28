9f88805748c183bd8c536e75266b5e28e8434c68
Gabriel-RQ <123425138+Gabriel-RQ@users.noreply.github.com>
2026-01-28 10:19:21 -0300

    update: adiciona tela de carregamento breve

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,20 @@
 <script lang="ts">
-  import { Header, Main, Footer } from "$lib/sections";
+  import { Header, Main, Footer, LoadingScreen } from "$lib/sections";
+  import { onMount } from "svelte";
+
+  let isLoading = $state(true);
+  const loadingDuration = 1200;
+
+  onMount(() => {
+    const timer = setTimeout(() => (isLoading = false), loadingDuration);
+
+    return () => clearTimeout(timer);
+  });
 </script>
+
+{#if isLoading}
+  <LoadingScreen />
+{/if}
 
 <Header />
 
